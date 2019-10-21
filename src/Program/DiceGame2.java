804afe7d388a1ebb1a0641aeafd9d0127d8580fd
peterrevsbech <peter.revsbech@gmail.com>
2019-10-21 11:59:17 +0200

    Lavet lidt testkode i main

--- src/Program/DiceGame2.java
+++ src/Program/DiceGame2.java
@@ -3,6 +3,11 @@ package Program;
 public class DiceGame2 {
     public static void main(String[] args) {
 
+
+        DiceCup kop = new DiceCup(2,6);
+        kop.rollDice();
+        System.out.println(kop.getDieSum());
+
     }
 
 
