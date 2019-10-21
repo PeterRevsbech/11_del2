8f1d6e5c3c623dd138c87976846bd86306db24ca
Dalmos87 <dalmozdk@gmail.com>
2019-10-21 11:41:56 +0200

    Vi har arbejdet med die klassen og lavet den første struktur

--- src/Program/Die.java
+++ src/Program/Die.java
@@ -1,4 +1,23 @@
 package Program;
+import java.util.*;
 
 public class Die {
+
+   private int faceValue = 1;
+   private int sideNumber = 6;
+
+
+    public Die(int i) {
+        sideNumber = i;
+    }
+    public void Roll(){
+        Random Ran = new Random();
+        faceValue = Ran.nextInt(sideNumber) + 1;
+    }
+    public int getFaceValue(){
+        return faceValue;
+    }
+    public void setFaceValue(int y){
+        faceValue = y;
+    }
 }
