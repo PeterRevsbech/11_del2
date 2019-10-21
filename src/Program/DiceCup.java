d987f5141d22933e01e2fdfeae23be6a1fa9486e
peterrevsbech <peter.revsbech@gmail.com>
2019-10-21 11:43:42 +0200

    Har nu også getters og setter

--- src/Program/DiceCup.java
+++ src/Program/DiceCup.java
@@ -1,4 +1,43 @@
 package Program;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class DiceCup {
+    private int diceNumber = 1;
+    private int dieSum = 0;
+    private List<Die> dieList = new LinkedList<Die>();
+
+    public DiceCup(int diceNumber, int diceSideNumber) {
+        this.diceNumber = diceNumber;
+        for (int i =1; i<=diceNumber;i++) {
+            dieList.add(new Die());
+        }
+    }
+
+    public void rollDice() {
+        int sum=0;
+        for (int i=0; i < diceNumber; i++) {
+            dieList.get(i).roll();
+            sum =+ dieList.get(i).getFaceValue();
+        }
+        this.dieSum = sum;
+    }
+
+    public int getDiceNumber() {
+        return diceNumber;
+    }
+
+    public void setDiceNumber(int diceNumber) {
+        this.diceNumber = diceNumber;
+    }
+
+    public int getDieSum() {
+        return dieSum;
+    }
+
+    public void setDieSum(int dieSum) {
+        this.dieSum = dieSum;
+    }
 }
