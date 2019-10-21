2281bf5e089fb044ce2558e47763415d34f113e7
Dalmos87 <dalmozdk@gmail.com>
2019-10-21 14:06:19 +0200

    Vi har skrevet metoden playturn og lavet noget test script i main.

--- src/program/Main.java
+++ src/program/Main.java
@@ -4,9 +4,17 @@ public class Main {
     public static void main(String[] args) {
 
 
-        DiceCup kop = new DiceCup(2,6);
+        DiceCup kop = new DiceCup(2,6 );
         kop.rollDice();
         System.out.println(kop.getDieSum());
+
+        Player spiller = new Player("Svend",3000);
+        for (int i=1; i<=10; i++) {
+            System.out.println(spiller.getPoint());
+            spiller.playTurn(6,2);
+
+        }
+
 
     }
 
--- src/program/Player.java
+++ src/program/Player.java
@@ -5,13 +5,68 @@ public class Player {
     private String name;
 
     public Player(String name,int pointStart){
-        pointStart=point;
+        this.point =pointStart;
         this.name = name;
 
     }
 
-    public void playTurn(){
-        
+    public void playTurn(int sideNumber1, int diceNumber1){
+        while(true) {
+            System.out.println("Det er " + this.getName() + "´s tur.");
+            DiceCup DC = new DiceCup(diceNumber1, sideNumber1);
+            DC.rollDice();
+            System.out.println(this.getName() + " slog " + DC.getDieSum());
+            switch (DC.getDieSum()) {
+                case 2:
+                    this.setPoint(this.getPoint() + 250);
+                    System.out.println("Du ramte Tower: +250 point!");
+                    break;
+                case 3:
+                    this.setPoint(this.getPoint() - 100);
+                    System.out.println("Du ramte Crater: -100 point!");
+                    break;
+                case 4:
+                    this.setPoint(this.getPoint() + 100);
+                    System.out.println("Du ramte Palace Gates: +100 point!");
+                    break;
+                case 5:
+                    this.setPoint(this.getPoint() - 20);
+                    System.out.println("Du ramte Cold Desert: -20 point!");
+                    break;
+                case 6:
+                    this.setPoint(this.getPoint() + 180);
+                    System.out.println("Du ramte Walled City: +180!");
+                    break;
+                case 7:
+                    this.setPoint(this.getPoint());
+                    System.out.println("Du ramte Monastery: 0 point!");
+                    break;
+                case 8:
+                    this.setPoint(this.getPoint() - 70);
+                    System.out.println("Du ramte Black Cave: -70 point!");
+                    break;
+                case 9:
+                    this.setPoint(this.getPoint() + 60);
+                    System.out.println("Du ramte Huts in the mountain: +60 point!");
+                    break;
+                case 10:
+                    this.setPoint(this.getPoint() - 80);
+                    System.out.println("Du ramte The Werewall: -80 point! Du får en ekstra tur!");
+                    break;
+                case 11:
+                    this.setPoint(this.getPoint() - 50);
+                    System.out.println("Du ramte The Pitts: -50 point!");
+                    break;
+                case 12:
+                    this.setPoint(this.getPoint() + 650);
+                    System.out.println("Du ramte Goldmine: +650 point!");
+                    break;
+            }
+            if(DC.getDieSum() != 10){
+                break;
+            }
+
+        }
     }
 
     public void setPoint(int point) {
@@ -29,4 +84,6 @@ public class Player {
     public String getName() {
         return name;
     }
+
+
 }
